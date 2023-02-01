63de84006bc1949c727a5352e21f2f3e780f0f21
abiryusuf <yusuf_ctg@yahoo.com>
2023-02-01 00:50:19 -0500

    done for today

--- src/main/java/com/core_java/Methods.java
+++ src/main/java/com/core_java/Methods.java
@@ -1,4 +1,14 @@
 package com.core_java;
 
 public class Methods {
+    public static void main(String[] args) {
+       myFun();
+       myFun_1("");
+    }
+    static void myFun(){
+        System.out.println("I am abir");
+    }
+    static void myFun_1(String name){
+        System.out.println(name + "First");
+    }
 }
